a78c75c09a87f87e321f61dbf70d4625d8d17be2
Gheorghe Pinzaru <feross95@gmail.com>
2020-02-25 14:47:11 +0300

    Use new API to declare navigation

--- src/status_im/subs.cljs
+++ src/status_im/subs.cljs
@@ -197,6 +197,11 @@
 ;;GENERAL ==============================================================================================================
 
 (re-frame/reg-sub
+ :multiaccount/logged-in?
+ (fn [db]
+   (multiaccounts.model/logged-in? {:db db})))
+
+(re-frame/reg-sub
  :connection-stats
  :<- [:desktop/desktop]
  (fn [desktop _]
